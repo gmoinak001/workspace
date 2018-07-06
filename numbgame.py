6b3bdc7c90c387a31b893683a1265d3416b15abf
Moinak Ghosh <gmoinak50689@gmail.com>
2018-07-07 03:50:45 +0530

    new updates lets user to set the number of test cases to be prepared.

--- numbgame.py
+++ numbgame.py
@@ -1,11 +1,11 @@
-t = input("Enter the number of test-cases:");
-d = t;
-number = input("Enter the variable A for this test case:");
-a = []
-for i in str(number)[::-1]:
-     digit = int(i)
-     a.append(int(digit))
-a.reverse()
-import itertools
-for permutation in itertools.permutations(a):
-    print (permutation);
+t = int(input("Enter the number of test-cases:"));
+for x in range(0, t):
+    number = input("Enter the variable A for this test case:");
+    a = []
+    for i in str(number)[::-1]:
+        digit = int(i)
+        a.append(int(digit))
+        a.reverse()
+        import itertools
+        for permutation in itertools.permutations(a):
+            print (permutation);
